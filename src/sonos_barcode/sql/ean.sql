61cfb7de1d7200f3dbe0ea856a6b6d216eb31b63
Stuart Fisher <stuart.fisher@esrf.fr>
2022-08-31 09:16:22 +0200

    uppercase

--- src/sonos_barcode/sql/ean.sql
+++ src/sonos_barcode/sql/ean.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `barcode` (
-    `barcode` varchar(50) NOT NULL,
+    `barcode` VARCHAR(50) NOT NULL,
     `entity` VARCHAR(250) NOT NULL,
     PRIMARY KEY (`barcode`),
     UNIQUE KEY (`barcode`)
