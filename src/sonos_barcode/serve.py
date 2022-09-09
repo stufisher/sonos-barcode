b84f31bf7112b4901ebf4780ea6e804fb29cac28
Stu Fisher <contact@roygbiv.org.uk>
2022-09-09 12:45:27 +0200

    add args to launcher

--- src/sonos_barcode/serve.py
+++ src/sonos_barcode/serve.py
@@ -1,8 +1,24 @@
+import argparse
 import uvicorn
 
 
 def run():
-    uvicorn.run("sonos_barcode.api:app", host="0.0.0.0", port=8001, reload=True)
+    parser = argparse.ArgumentParser(description="Sonos Barcode server")
+    parser.add_argument(
+        "-p", "--port", dest="port", default=8001, help="REST server port", type=int
+    )
+    parser.add_argument(
+        "--reload",
+        action="store_true",
+        dest="reload",
+        default=False,
+        help="Enable reloader for debugging",
+    )
+    args = parser.parse_args()
+
+    uvicorn.run(
+        "sonos_barcode.api:app", host="0.0.0.0", port=args.port, reload=args.reload
+    )
 
 
 if __name__ == "__main__":
